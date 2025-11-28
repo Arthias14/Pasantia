fd1116fb08a564dbde80706412673aa8197a0096
Arthias14 <gonzalezboscoelias@gmail.com>
2025-11-28 09:29:34 -0300

    Commit Inicial

---
+++ js/bronzeadventures.js
@@ -0,0 +1,6 @@
+function seleccionarLommianJugador(){
+    alert("Seleccionaste a tu Loomian")
+}
+
+let botonLoomianJugador = document.getElementById('boton-loomian')
+botonLoomianJugador.addEventListener('click', seleccionarLommianJugador)
